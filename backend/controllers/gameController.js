1c32c4377397629d6c916e03a5603dd435ba03c6
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2024-06-14 19:22:42 -0400

    Genres EndPoint

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,4 +1,4 @@
-// Search games based on searchQuery will return JSON of id, alternative_name, game (int), name, published_at
+// Search for games based on search query
 export const searchGames = async (req, res) => {
   try {
     const url = "https://api.igdb.com/v4/search";
@@ -43,8 +43,8 @@ export const artWorkByID = async (req, res) => {
   }
 };
 
-//Get video by gameID
-export const videoByID = async (req, res) => {
+//Get video game info by gameID
+export const gameByID = async (req, res) => {
   try {
     const url = "https://api.igdb.com/v4/games";
     const gameID = req.params.id; // Game ID from request params
@@ -54,7 +54,7 @@ export const videoByID = async (req, res) => {
     const response = await fetch(url, {
       method: "POST",
       headers: headers, // Headers are added by addHeaders middleware
-      body: `fields *; where id = ${gameID};`, // Get videos based on gameID
+      body: `fields genres; where id = ${gameID};`, // Get videos based on gameID
     });
 
     const data = await response.json();
@@ -62,6 +62,44 @@ export const videoByID = async (req, res) => {
     res.status(200).json(data);
   } catch (error) {
     console.log("Error in videoByID:", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Get genres of video game by genreID
+export const genresByID = async (req, res) => {
+  try {
+    const url = "https://api.igdb.com/v4/genres";
+
+    const genreIDs = req.params.id
+      .split(",")
+      .map((id) => parseInt(id.trim(), 10)); // Parse Genre IDs from request params
+    const headers = req.headers;
+
+    // Fetch genres based on genreID
+    const fetchGenresByID = async (id) => {
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: headers, // Headers are added by addHeaders middleware
+          body: `fields name; where id = ${id};`, // Get genres based on genreID
+        });
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.log("Error in fetchGenresByID:", error.message);
+        return { message: error.message };
+      }
+    };
+
+    const genreData = await Promise.all(genreIDs.map(fetchGenresByID)); // Fetch genres for each genreID
+
+    // Combine the results from multiple API calls and extract the names
+    const combinedGenreData = genreData.flat().map((genre) => genre.name);
+
+    res.status(200).json(combinedGenreData);
+  } catch (error) {
+    console.log("Error in genresByID:", error.message);
     res.status(500).json({ message: error.message });
   }
 };
--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -4,10 +4,12 @@ import addHeaders from "../middleware/addHeaders.js";
 
 const router = express.Router();
 
-router.post("/search", addHeaders, gameController.searchGames); // POST /api/games/search (search for games by search query)
+router.post("/search", addHeaders, gameController.searchGames); //  (search for games by search query)
 
-router.post("/artwork/:id", addHeaders, gameController.artWorkByID); // GET /api/games/artwork/:id (get covert art of games by game ID)
+router.post("/artwork/:id", addHeaders, gameController.artWorkByID); //  (get covert art of games by game ID)
 
-router.post("/:id", addHeaders, gameController.videoByID); // GET /api/games/:id (get video of games by game ID)
+router.post("/:id", addHeaders, gameController.gameByID); // (get video of games by game ID)
+
+router.post("/genres/:id", addHeaders, gameController.genresByID); //  (get all genres of games by genre ID)
 
 export default router;
