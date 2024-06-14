7b57a5295178a5f6c474efe609bc6f7cd2caed11
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2024-06-14 14:54:54 -0400

    Covert Art Endpoint

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,21 +1,46 @@
+// Search games based on searchQuery will return JSON of id, alternative_name, game (int), name, published_at
 export const searchGames = async (req, res) => {
   try {
-
     const url = "https://api.igdb.com/v4/search";
-    const searchQuery = req.body.searchQuery;
+    const searchQuery = req.body.searchQuery; // Search query from request body
 
     const headers = req.headers;
 
     const reponse = await fetch(url, {
       method: "POST",
       headers: headers, // Headers are added by addHeaders middleware
-      body: `fields *; search "${searchQuery}"; limit 10;`,
+      body: `fields *; search "${searchQuery}"; limit 10;`, // Search for games based on searchQuery and limit to 10 results
     });
 
     const data = await reponse.json();
     res.status(200).json(data);
   } catch (error) {
     console.log("Error in searchGames:", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get covert at by game ID will return JSON of url
+export const artWorkByID = async (req, res) => {
+  try {
+    const url = "https://api.igdb.com/v4/covers";
+    const gameID = req.params.id; // Game ID from request params
+    console.log("Game ID:", gameID);
+
+    const headers = req.headers;
+    console.log("Headers:", headers);
+
+    const reponse = await fetch(url, {
+      method: "POST",
+      headers: headers, // Headers are added by addHeaders middleware
+      body: `fields url; where game = ${gameID};`, // Get artworks based on gameID and limit to 10 results
+    });
+
+    const data = await reponse.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.log("Error in artWorkByID:", error.message);
     res.status(500).json({ message: error.message });
   }
 };
--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -4,6 +4,8 @@ import addHeaders from "../middleware/addHeaders.js";
 
 const router = express.Router();
 
-router.post("/search", addHeaders, gameController.searchGames); // POST /api/games/search (search for games)
+router.post("/search", addHeaders, gameController.searchGames); // POST /api/games/search (search for games by search query)
+
+router.post("/artwork/:id", addHeaders, gameController.artWorkByID); // GET /api/games/artwork/:id (get covert art of games by game ID)
 
 export default router;
