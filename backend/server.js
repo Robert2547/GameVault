0bfd61660af3e039df22745b2ae5c9f3bc5c8e90
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2024-06-15 15:31:16 -0400

    userProfile endpoint

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,12 @@ import connectDB from "./db/connectToMongoDB.js";
 import { checkToken } from "./middleware/checkToken.js";
 import { protectRoute } from "./middleware/protectRoute.js";
 import cookieParser from "cookie-parser";
+import userProfileRoutes from "./routes/userProfileRoutes.js";
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
@@ -23,7 +20,9 @@ app.use(cookieParser()); // Parse cookies
 // Routes
 app.use("/api/auth", authRoutes); // Handle signup, login, logout
 app.use("/api/game", protectRoute, checkToken, gameRoutes); // Handle IGDB API requests (Game Catalog API)
+app.use("/api/profile", userProfileRoutes); // Handle user profile requests
 
 app.listen(PORT, () => {
   connectDB();
+  console.log(`Server is listening on port ${PORT}`);
 });
